76fb8a4673d2e54b95d9b584f21b60f61bdd6ab6
David Derby <dderby@server.fake>
2014-04-05 18:19:53 -0500

    Fix import to handle multi-line Twitter posts correctly

--- enunciado/import_tweets.sh
+++ enunciado/import_tweets.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mongo tweets --eval "db.dropDatabase()"
-cat debate08_sentiment_tweets.tsv | tr '\r' '\n' | grep -v ^# | mongoimport -d tweets -c tweets --type tsv --headerline
+cat debate08_sentiment_tweets.tsv | sed 's/\r9/\n9/g' | sed 's/#\r/#\n/g' | grep -v ^# | mongoimport -d tweets -c tweets --type tsv --headerline
